8dd204916d84b4f557575407686b32e54a43722d
wfernandezfurcal <ferfwill@gmail.com>
2024-02-10 23:35:10 -0400

    V1.0.3
    Toggle symbols and numbers done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+//Numbers 52 to 61
+//Symbols 62 to 90
 
 let inputOneEle = document.getElementById("inputOne");
 let inputTwoEle = document.getElementById("inputTwo");
@@ -17,7 +19,6 @@ function generatePassword() {
 
     //Without Symbols and numbers
     if (!symbolsCheck.checked && !numbersCheck.checked){
-        console.log("Without Symbols and numbers");
         for (let i = 0; i < 15; i++){
             characterOne = characters[Math.floor(Math.random() * 51)];
             characterTwo = characters[Math.floor(Math.random() * 51)];
@@ -29,29 +30,33 @@ function generatePassword() {
     //only symbols
     else if (symbolsCheck.checked && !numbersCheck.checked){
         for (let i = 0; i < 15; i++){
-            
-            characterOne = characters[Math.floor(Math.random() * 51)];
-            characterTwo = characters[Math.floor(Math.random() * 51)];
+
+            characterOne = characters[characterFilter()];
+            characterTwo = characters[characterFilter()];
     
             firstPasswordGenerated += characterOne;
             secondPasswordGenerated += characterTwo;
         }
     }
-    
-
     //only numbers
-
-    //else
-
-
-
-    // for (let i = 0; i < 15; i++){
-    //     characterOne = characters[Math.floor(Math.random() * 91)];
-    //     characterTwo = characters[Math.floor(Math.random() * 91)];
-
-    //     firstPasswordGenerated += characterOne;
-    //     secondPasswordGenerated += characterTwo;
-    // }
+    else if (!symbolsCheck.checked && numbersCheck.checked){
+        console.log("only numbers");
+        for (let i = 0; i < 15; i++){
+            characterOne = characters[Math.floor(Math.random() * 61)];
+            characterTwo = characters[Math.floor(Math.random() * 61)];
+    
+            firstPasswordGenerated += characterOne;
+            secondPasswordGenerated += characterTwo;
+        }
+    }else{ //Both on
+        for (let i = 0; i < 15; i++){
+            characterOne = characters[Math.floor(Math.random() * 91)];
+            characterTwo = characters[Math.floor(Math.random() * 91)];
+    
+            firstPasswordGenerated += characterOne;
+            secondPasswordGenerated += characterTwo;
+        }
+    }
 
     inputOneEle.value = firstPasswordGenerated;
     inputTwoEle.value = secondPasswordGenerated;
@@ -65,3 +70,15 @@ function copyToClipboard(element) {
      // Copy the text inside the text field
     navigator.clipboard.writeText(element.value);
   }
+
+  function characterFilter(){
+    let filteredCharter = Math.floor(Math.random() * 91);
+    
+    while (filteredCharter > 51 && filteredCharter < 62){
+        filteredCharter = Math.floor(Math.random() * 91);
+    }
+
+    return filteredCharter;
+
+  }
+  
